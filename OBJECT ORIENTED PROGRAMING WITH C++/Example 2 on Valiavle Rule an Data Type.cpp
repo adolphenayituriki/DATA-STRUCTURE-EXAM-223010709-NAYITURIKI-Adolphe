0936470f396dc3a79df2c895bfa85fa10cb8bd38
Adolphe NAYITURIKI <162124147+adolphenayituriki@users.noreply.github.com>
2025-02-21 08:56:32 +0200

    Example 2 on Valiavle Rule an Data Type.cpp

--- OBJECT ORIENTED PROGRAMING WITH C++/Example 2 on Valiavle Rule an Data Type.cpp
+++ OBJECT ORIENTED PROGRAMING WITH C++/Example 2 on Valiavle Rule an Data Type.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-      int age = 20;  // Integer variable
-      double price = 19.99;  // Double variable
-      char grade = 'A';  // Character variable
-      string name = "Alice";  // String variable
+      int age = 20;
+      double price = 19.99;
+      char grade = 'A';  
+      string name = "Alice";
 
       cout << "Name: " << name << endl;
       cout << "Adolphe: " << age << endl;
